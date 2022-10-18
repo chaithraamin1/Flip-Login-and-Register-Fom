b5033ddfc70f479827a9548cd8e1cfecb45a19d0
chai <chaithra.amin98@gmail.com>
2022-10-18 18:07:51 +0530

    flip card for login and register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'flip-login-and-registration-form';
+  ngOnInit(){
+
+  }
+  openSignup(){
+    var card:any=document.getElementById("card");
+    card.style.transform="rotateY(-180deg)";
+  }
+  openLogin(){
+    var card:any=document.getElementById("card");
+    card.style.transform="rotateY(0deg)";
+  }
+
 }
